f75ad7a99cf6191a6276a108ac2501433c746c26
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-27 14:55:02 -0300

    Update Info•bot.js

--- plugins/Info•bot.js
+++ plugins/Info•bot.js
@@ -5,21 +5,7 @@ handler.all = async function(m) {
 const chat = global.db.data.chats[m.chat];
 if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
-conn.reply(m.chat, `✿︎ ¡Hola! Soy Akari, en que puedo ayudarte hoy?\n\n✰ Usa *.menu* para ver mis comandos.`, m, )
-}
-
-if (chat.isBaneed) return
-if (/^akari|watanabe$/i.test(m.text)) {
-conn.reply(m.chat, `> *Hola soy AkariBot-MD y fui creada por*\n> *ianalejandrook15x* \n\n> Escribe *.menu* para ver mis comandos, recuerda no hagas spam a la bot, Disfrute de *AkariBot-MD*\n\nPσɯҽɾҽԃ Bყ Tҽαɱ SHιɳιɳɠ 🌙`, m, )
-}
-
-if (chat.isBaneed) return
-if (/^reglas$/i.test(m.text)) {
-conn.reply(m.chat, `*Hola si quieres unir a la bot deberas seguir las reglas basicas* 👇\n\n> No hacer spam a la bot\n\n> No llamar a la bot\n\n> El grupo debe de tener un minimo de 25 miembros o no sera otorgado el bot\n\n> No añadir a la bot sin permisos del creador \n\n> Wa.me/5493876639332\n\n> Disfrute de *AkariBot-MD Powered By Ianalejandrook15x*`, m, )
-}
-  
-if (/^ian$/i.test(m.text)) {
-conn.reply(m.chat, `Hola Ian es mi creador, si deseas unir a la bot puedes contactarte con el aqui te dejo su numero\n\n> wa.me/5493876639332`, m, )
+conn.reply(m.chat, `Hola este es Sylpha Bot, utiliza *.menu* para ver los comandos de la bot`, m, )
 }
 return !0;
 };
