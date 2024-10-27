120f357dde1178535fa980728769cee42d7ac548
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-27 14:49:03 -0300

    Update Herramientas•translate.js

--- plugins/Herramientas•translate.js
+++ plugins/Herramientas•translate.js
@@ -1,7 +1,7 @@
 import translate from '@vitalets/google-translate-api';
 import fetch from 'node-fetch';
 const handler = async (m, {args, usedPrefix, command}) => {
-  const msg = `👑 *Uso correcto del comando ${usedPrefix + command} (idioma) (texto)*\n*Ejemplo:*\n*${usedPrefix + command} es Hello*\n\n*Conoce los idiomas admitidos en:*\n*- https://cloud.google.com/translate/docs/languages*`;
+  const msg = `✦ *Uso correcto del comando ${usedPrefix + command} (idioma) (texto)*\n*Ejemplo:*\n*${usedPrefix + command} es Hello*\n\n*Conoce los idiomas admitidos en:*\n*- https://cloud.google.com/translate/docs/languages*`;
   if (!args || !args[0]) return m.reply(msg);
   let lang = args[0];
   let text = args.slice(1).join(' ');
@@ -38,7 +38,7 @@ const handler = async (m, {args, usedPrefix, command}) => {
       await m.react(done)
     } catch {
       await m.react(error)
-      await conn.reply(m.chat, 'ꕥ️ *Ocurrió Un Error*', m, );
+      await conn.reply(m.chat, '✦ *Ocurrió Un Error*', m, );
     }
   }
 };
