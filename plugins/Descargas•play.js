ff1a9643dd3f6454d447ebb7ea9778037229e5ba
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-27 14:03:48 -0300

    Update Descargas•play.js

--- plugins/Descargas•play.js
+++ plugins/Descargas•play.js
@@ -3,7 +3,7 @@ import yts from "yt-search";
 
 const handler = async (m, {conn, command, args, text, usedPrefix}) => {
 
-if (!text) return conn.reply(m.chat, `✿︎ *Ingrese el nombre de un video de YouTube*\n\nEjemplo, !${command} Nakano Nino Edit`,  m, , )
+if (!text) return conn.reply(m.chat, `✿︎ *Ingrese el nombre de un video de YouTube*\n\nEjemplo, !${command} Nakano Nino Edit`,  m, )
 
 conn.reply(m.chat, global.wait, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
