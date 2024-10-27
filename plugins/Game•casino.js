6b233c0bf63720bab8580156ddc55bfe870b8e20
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-27 14:18:25 -0300

    Update Game•casino.js

--- plugins/Game•casino.js
+++ plugins/Game•casino.js
@@ -21,7 +21,7 @@ return
 cooldowns[m.sender] = Date.now()
 count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].limit / buatall) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
 count = Math.max(1, count)
-if (args.length < 1) return conn.reply(m.chat, '✿︎ Ingresa la cantidad de ' + `💴 *Yenes*` + ' que deseas aportar contra' + ` *AkariBot-MD*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, )
+if (args.length < 1) return conn.reply(m.chat, '✿︎ Ingresa la cantidad de ' + `💴 *Yenes*` + ' que deseas aportar contra' + ` *Sylpha*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, )
 if (user.yenes >= count * 1) {
 user.yenes -= count * 1
 if (Aku > Kamu) {
