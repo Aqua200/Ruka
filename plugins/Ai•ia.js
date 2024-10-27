86460dad09ada54f4fe33534f4f4f7aad30d0240
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-26 21:17:21 -0300

    Update Ai•ia.js

--- plugins/Ai•ia.js
+++ plugins/Ai•ia.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 const isQuotedImage = m.quoted && (m.quoted.msg || m.quoted).mimetype && (m.quoted.msg || m.quoted).mimetype.startsWith('image/')
 const username = `${conn.getName(m.sender)}`
-const basePrompt = `Tu nombre es AkariBot-MD y parece haber sido creado por ianalejandrook15x. Tu versión actual es 1.0.1, Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, y te encanta aprender. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
+const basePrompt = `Tu nombre es Sylpha y parece haber sido creado por ianalejandrook15x. Tu versión actual es 1.0.1, Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, y te encanta aprender. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
 if (isQuotedImage) {
 const q = m.quoted
 const img = await q.download?.()
@@ -22,7 +22,7 @@ await conn.reply(m.chat, description, m, )
 console.error('✿︎ Error al analizar la imagen:', error)
 await conn.reply(m.chat, '✿︎ Error al analizar la imagen.', m, )}
 } else {
-if (!text) { return conn.reply(m.chat, `✿︎ *Ingrese su petición*\n✿︎ *Ejemplo de uso:* ${usedPrefix + command} Como hacer un avión de papel`, m, )}
+if (!text) { return conn.reply(m.chat, `✿︎ *Ingrese su petición*\n✿︎ *Ejemplo de uso:* ${usedPrefix + command} Haz un codigo para JavaScript`, m, )}
 await m.react('💬')
 try {
 const query = text
