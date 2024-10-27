cbbdaa974c63f3b697993ed3c1284d29e1dde4f3
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-27 15:41:26 -0300

    Update Sticker•sticker.js

--- plugins/Sticker•sticker.js
+++ plugins/Sticker•sticker.js
@@ -6,7 +6,7 @@ import { webp2png } from '../lib/webp2mp4.js'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 let text1 = `✡︎ → Solicitado por:\n✡︎ → Bot:\n✡︎ → Made By:`
-let text2 = `✧ ${nombre}\n✧ AkariBot\n✧ ianalejandrook15x`
+let text2 = `✧ ${nombre}\n✧ Sylpha\n✧ ianalejandrook15x`
 let stiker = false
 try {
 let q = m.quoted ? m.quoted : m
