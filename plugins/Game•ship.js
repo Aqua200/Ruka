319c91e55bf704c77dc4a273a0945caa21197eac
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-27 14:21:07 -0300

    Update Game•ship.js

--- plugins/Game•ship.js
+++ plugins/Game•ship.js
@@ -5,7 +5,7 @@ let [text1, ...text2] = text.split(' ')
 
 text2 = (text2 || []).join(' ')
 if (!text2) return conn.reply(m.chat, `✿︎ *Escribe el nombre de la segunda persona*`, m, )
-let love = `❤️ *${text1}* tu oportunidad de enamorarte de *${text2}* es de ${Math.floor(Math.random() * 100)}% 👩🏻‍❤️‍👨🏻`
+let love = `✧ *${text1}* tu oportunidad de enamorarte de *${text2}* es de ${Math.floor(Math.random() * 100)}% ⚜`
 
 m.reply(love, null, { mentions: conn.parseMention(love) })
 
