adab3defd776c40bbb2bca4f7ca9cffab79a6a4f
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-27 12:32:12 -0300

    Update Config•antilink.js

--- plugins/Config•antilink.js
+++ plugins/Config•antilink.js
@@ -10,18 +10,18 @@ let bang = m.key.id
 let bot = global.db.data.settings[this.user.jid] || {}
 const isGroupLink = linkRegex.exec(m.text)
 const grupo = `https://chat.whatsapp.com`
-if (isAdmin && chat.antiLink && m.text.includes(grupo)) return conn.reply(m.chat, `🏷 *Hey!! el anti link esta activo pero eres admin, ¡salvado!*`, m, , )
+if (isAdmin && chat.antiLink && m.text.includes(grupo)) return conn.reply(m.chat, `*# El anti link se encuentra activo, pero eres admin`, m, , )
 if (chat.antiLink && isGroupLink && !isAdmin) {
 if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
 if (m.text.includes(linkThisGroup)) return !0
 }
-await conn.reply(m.chat, `📎 *¡Enlace detectado!*\n\n*${await this.getName(m.sender)} mandaste un enlace prohibido por lo cual seras eliminado*`, m, , )
-if (!isBotAdmin) return conn.reply(m.chat, `🌼 *No soy admin, no puedo eliminar intrusos*`, m, , )
+await conn.reply(m.chat, `> ❀ *Enlace detectado*\n\n*${await this.getName(m.sender)} seras eliminado por antilink*`, m, , )
+if (!isBotAdmin) return conn.reply(m.chat, `❀ *# EL bot no es administrador no podra eliminar a intrusos*`, m, , )
 if (isBotAdmin) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-} else if (!bot.restrict) return conn.reply(m.chat, `*¡Esta característica esta desactivada!*`, m, , )
+} else if (!bot.restrict) return conn.reply(m.chat, `*# El comando se encuentra desactivado*`, m, , )
 }
 return !0
 
